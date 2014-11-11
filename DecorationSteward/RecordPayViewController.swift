c7363f2c62e352b3f4c7fbe297afb6661c8c27c4
RainbowMango <qdurenhongcai@163.com>
2014-11-11 14:23:29 +0800

    Add navigation bar item "Done"

--- DecorationSteward/RecordPayViewController.swift
+++ DecorationSteward/RecordPayViewController.swift
@@ -12,6 +12,9 @@ import UIKit
 
 class RecordPayViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate, UIPickerViewDelegate {
 
+    @IBOutlet weak var payNavigationItem: UINavigationItem!
+    @IBOutlet weak var rightBarButton: UIBarButtonItem!
+    
     @IBOutlet weak var payTextField: UITextField!
     @IBOutlet weak var brandTextField: UITextField!
     @IBOutlet weak var phoneTextField: UITextField!
@@ -29,6 +32,9 @@ class RecordPayViewController: UIViewController, UITextFieldDelegate, UITextView
         self.addrTextField.delegate = self
         self.commentTextView.delegate = self
         self.catagoryPickerView.delegate = self
+        
+        // 设置Navigation right bar item
+        self.rightBarButton.title = "完成"
         
         // Do any additional setup after loading the view.
     }
@@ -50,6 +56,18 @@ class RecordPayViewController: UIViewController, UITextFieldDelegate, UITextView
         if self.commentTextView.text == "<点击输入备注>" {
             self.commentTextView.text = nil
         }
+    }
+    
+    // 用户完成表单输入
+    @IBAction func finishRecord(sender: AnyObject) {
+        println("finishRecord() 用户完成输入")
+        
+        // 取得表单信息
+        println("已花费：\(self.payTextField.text)")
+        println("品牌：\(self.brandTextField.text)")
+        println("商家电话：\(self.phoneTextField.text)")
+        println("商家地址：\(self.addrTextField.text)")
+        println("备注：\(self.commentTextView.text)")
     }
     
     /*
