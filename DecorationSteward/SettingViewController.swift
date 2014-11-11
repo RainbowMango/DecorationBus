dc9a6c804451827b7d0efaba215dab030a00e8d4
RainbowMango <qdurenhongcai@163.com>
2014-11-11 20:06:19 +0800

    Add setting View

---
+++ DecorationSteward/SettingViewController.swift
@@ -0,0 +1,61 @@
+//
+//  SettingViewController.swift
+//  DecorationSteward
+//
+//  Created by ruby on 14-11-11.
+//  Copyright (c) 2014年 ruby. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var settingTableView: UITableView!
+    
+    var settingItems = ["修改类别", "提交反馈", "关于装修管家"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 设置tableView代理和数据源，否则无法显示，也可以在IB中连线
+        settingTableView.delegate = self
+        settingTableView.dataSource = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // 设置显示cell的数目
+    // TODO：改方法会被调用两次，使显示的sell数目是预期的两倍
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        println("显示cell数目：\(self.settingItems.count)")
+        return settingItems.count
+    }
+
+    // 设置每个cell的内容
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        cell.textLabel.text = self.settingItems[indexPath.row]
+        
+        return cell
+    }
+    
+    // 设定选中时的动作
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        println("选中cell with index\(indexPath.row)")
+        
+        switch indexPath.row {
+        case 0:
+            //self.navigationController?.pushViewController(CatagoryPrimeMangeViewController(), animated: true)
+            println("暂未实现")
+        case 1:
+            println("暂未实现")
+        case 2:
+            println("暂未实现")
+        default:
+            println("不存在该cell index")
+        }
+    }
+}
