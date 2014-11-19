fc0efdd604f6a98c92d1e6b6cb44b381a8d24191
RainbowMango <qdurenhongcai@163.com>
2014-11-19 21:41:16 +0800

    refactor record view

--- DecorationSteward/RecordPayViewController.swift
+++ DecorationSteward/RecordPayViewController.swift
@@ -15,30 +15,36 @@ class RecordPayViewController: UIViewController, UITextFieldDelegate, UITextView
     @IBOutlet weak var payNavigationItem: UINavigationItem!
     @IBOutlet weak var rightBarButton: UIBarButtonItem!
     
-    @IBOutlet weak var payTextField: UITextField!
-    @IBOutlet weak var brandTextField: UITextField!
+    @IBOutlet weak var moneyTextField: UITextField!
+    @IBOutlet weak var categoryTextField: UITextField!
+    @IBOutlet weak var shopTextField: UITextField!
     @IBOutlet weak var phoneTextField: UITextField!
     @IBOutlet weak var addrTextField: UITextField!
     @IBOutlet weak var commentTextView: UITextView!
-    @IBOutlet weak var catagoryPickerView: UIPickerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // 设置空间的代理到本Controller，也可以在IB中连线
-        self.payTextField.delegate = self
-        self.brandTextField.delegate = self
-        self.phoneTextField.delegate = self
-        self.addrTextField.delegate = self
-        self.commentTextView.delegate = self
-        self.catagoryPickerView.delegate = self
+        initView()
         
+        // 设置空间的代理到本Controller，也可以在IB中连线
+//        self.payTextField.delegate = self
+//        self.brandTextField.delegate = self
+//        self.phoneTextField.delegate = self
+//        self.addrTextField.delegate = self
+//        self.commentTextView.delegate = self
+//        self.catagoryPickerView.delegate = self
+        
+    }
+
+    func initView() {
         // 设置Navigation right bar item
         self.rightBarButton.title = "完成"
         
-        // Do any additional setup after loading the view.
+        // 设置textView边框
+        self.commentTextView.layer.borderWidth = 1
+        self.commentTextView.layer.borderColor = UIColor.grayColor().CGColor
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -53,9 +59,9 @@ class RecordPayViewController: UIViewController, UITextFieldDelegate, UITextView
 
     // 用户首次开始输输入时清空原内容
     func textViewDidBeginEditing(textView: UITextView) {
-        if self.commentTextView.text == "<点击输入备注>" {
-            self.commentTextView.text = nil
-        }
+//        if self.commentTextView.text == "<点击输入备注>" {
+//            self.commentTextView.text = nil
+//        }
     }
     
     // 用户完成表单输入
@@ -63,21 +69,12 @@ class RecordPayViewController: UIViewController, UITextFieldDelegate, UITextView
         println("finishRecord() 用户完成输入")
         
         // 取得表单信息
-        println("已花费：\(self.payTextField.text)")
-        println("品牌：\(self.brandTextField.text)")
-        println("商家电话：\(self.phoneTextField.text)")
-        println("商家地址：\(self.addrTextField.text)")
+        println("金额：\(self.moneyTextField.text)")
+        println("类别：\(self.categoryTextField.text)")
+        println("商家：\(self.shopTextField.text)")
+        println("电话：\(self.phoneTextField.text)")
+        println("地址：\(self.addrTextField.text)")
         println("备注：\(self.commentTextView.text)")
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
