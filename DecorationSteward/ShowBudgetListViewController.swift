f0ba0823131486abd50fdcdfeef344241a751ff7
RainbowMango <qdurenhongcai@163.com>
2014-11-24 22:20:51 +0800

    add show budget view

--- DecorationSteward/ShowBudgetListViewController.swift
+++ DecorationSteward/ShowBudgetListViewController.swift
@@ -1,46 +1,46 @@
 //
-//  ShowPayListViewController.swift
+//  ShowBudgetListViewController.swift
 //  DecorationSteward
 //
-//  Created by ruby on 14-11-23.
+//  Created by ruby on 14-11-24.
 //  Copyright (c) 2014年 ruby. All rights reserved.
 //
 
 import UIKit
 
-class ShowPayListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
+class ShowBudgetListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
     @IBOutlet weak var deTailTableView: UITableView!
     
-    var cellReuseIdentifier: String = "payCell"
-    var orders: Array<OrderItem> = OrderArchiver().getOrdesFromUserDefault()
+    var cellReuseIdentifier: String = "budgetCell"
+    var budgets: Array<BudgetItem> = BudgetArchiver().getBudgetsFromUserDefault()
     
     override func viewDidLoad() {
         println("viewDidLoad() \(self)")
         super.viewDidLoad()
         
         initTableView()
-
-        orders = OrderArchiver().getOrdesFromUserDefault()
+        
+        budgets = BudgetArchiver().getBudgetsFromUserDefault()
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
     // MARK: Init functions
     
     // 初始化table view
@@ -52,15 +52,15 @@ class ShowPayListViewController: UIViewController, UITableViewDataSource, UITabl
     
     // 每个section显示行数
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        println("显示行数: \(self.orders.count)")
-        return self.orders.count
+        println("显示行数: \(self.budgets.count)")
+        return self.budgets.count
     }
     
     // 设置cell内容
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier(cellReuseIdentifier, forIndexPath: indexPath) as UITableViewCell
         cell.accessoryType = UITableViewCellAccessoryType.DetailDisclosureButton
-        cell.textLabel.text = self.orders[indexPath.row].category + "   " + self.orders[indexPath.row].money
+        cell.textLabel.text = self.budgets[indexPath.row].category + "   " + self.budgets[indexPath.row].money
         
         return cell
     }
