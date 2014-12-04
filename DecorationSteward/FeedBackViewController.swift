b178c4ea73ee7d269ddb08b16127d8b33552f80a
duan <duan@adminisordemini.home>
2014-12-04 19:22:46 +0800

    反馈信息更新

--- DecorationSteward/FeedBackViewController.swift
+++ DecorationSteward/FeedBackViewController.swift
@@ -9,39 +9,168 @@
 import UIKit
 import MessageUI
 
-class FeedBackViewController: UIViewController, MFMailComposeViewControllerDelegate {
-
-    @IBOutlet weak var feedbackTextView: UITextView!
+class FeedBackViewController: UIViewController, MFMailComposeViewControllerDelegate,UIScrollViewDelegate,UITextViewDelegate
+{
     
-    override func viewDidLoad() {
+    var deviceHeight:CGFloat = 0.0;
+    var feedbackTextView_ = UITextView();
+    var placeholderLabel_ = UILabel();
+    let feedbackScrollView_ = UIScrollView();
+    let pointMessageView_ = PointMessageViewController();
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+        
+        self.initDeviceHeight();
+        self.addFeedBackScrollview();
+        self.addFeedbackTextView();
+        self.addPlaceholderLabel();
+        self.addPointMessageView();
     }
     
-    @IBAction func DoneButton(sender: AnyObject) {
-        println("DoneButton() 用户反馈: \(self.feedbackTextView.text)")
+    func initDeviceHeight()
+    {
+        if (DeviceSystemVersion>7.0)
+        {
+            deviceHeight = 60.0;
+        }else
+        {
+            deviceHeight = 0.0;
+        }
+    }
+    
+    
+    @IBAction func DoneButton(sender: AnyObject)
+    {
+        println("DoneButton() 用户反馈: \(feedbackTextView_.text)")
         
         // 发送邮件
         var mailController = MFMailComposeViewController()
         mailController.mailComposeDelegate = self
         
-        mailController.setToRecipients(["qdurenhongcai@163.com"])
+        mailController.setToRecipients(["plng322@163.com"])
         mailController.setSubject("Test Subject")
-        mailController.setMessageBody("Test Body", isHTML: true)
+        mailController.setMessageBody(feedbackTextView_.text, isHTML: false)
         self.presentViewController(mailController, animated: true, completion: nil)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    //添加消息弹出界面
+    func addPointMessageView()
+    {
+        let viewSize:CGSize = self.view.frame.size;
+        let viewHeight:CGFloat = viewSize.height;
+        let viewWidth:CGFloat = viewSize.width;
+        
+        let pointMessageViewRect:CGRect = CGRectMake((viewWidth - 120) / 2,
+            (viewHeight - 50) / 2,
+            120,
+            50);
+        pointMessageView_.view.frame = pointMessageViewRect;
+        
+        self.view.addSubview(pointMessageView_.view);
+        
     }
-    */
-
+    
+    //添加ScrollView
+    func addFeedBackScrollview()
+    {
+        let viewSize:CGSize = self.view.frame.size;
+        let viewHeight:CGFloat = viewSize.height;
+        let viewWidth:CGFloat = viewSize.width;
+        
+        let feedBackScrollviewRect:CGRect = CGRectMake(0,0,viewWidth,viewHeight-deviceHeight);
+        feedbackScrollView_.frame = feedBackScrollviewRect;
+        feedbackScrollView_.delegate = self;
+        feedbackScrollView_.contentSize = CGSizeMake(viewWidth, viewHeight-deviceHeight+1);
+        feedbackScrollView_.backgroundColor = UIColor.clearColor();
+        
+        self.view.addSubview(feedbackScrollView_);
+    }
+    
+    //添加TextView
+    func addFeedbackTextView()
+    {
+        let viewSize:CGSize = self.view.frame.size;
+        let viewHeight:CGFloat = viewSize.height;
+        let viewWidth:CGFloat = viewSize.width;
+        
+        let feedbackTextViewRect:CGRect = CGRectMake(0, 0, viewWidth, 200);
+        feedbackTextView_.frame = feedbackTextViewRect;
+        feedbackTextView_.delegate = self;
+        feedbackTextView_.backgroundColor = UIColor.clearColor();
+        feedbackTextView_.font = UIFont.systemFontOfSize(16.0);
+        
+        feedbackScrollView_.addSubview(feedbackTextView_);
+        feedbackTextView_.becomeFirstResponder();
+    }
+    
+    //添加Label
+    func addPlaceholderLabel()
+    {
+        let placeholderLabelRect:CGRect = CGRectMake(3, 0, 300, 38);
+        placeholderLabel_.frame = placeholderLabelRect;
+        placeholderLabel_.font = UIFont.systemFontOfSize(14.0);
+        placeholderLabel_.backgroundColor = UIColor.clearColor();
+        placeholderLabel_.textColor = UIColor.lightGrayColor();
+        placeholderLabel_.text = "请输入您的反馈意见(字数200字以内)";
+        
+        feedbackScrollView_.addSubview(placeholderLabel_);
+    }
+    
+    //MARK: -UITextView Delegate
+    func textViewDidChange(textView: UITextView)
+    {
+        let newLength = countElements(textView.text);
+        if (newLength == 0)
+        {
+            placeholderLabel_.text = "请输入您的反馈意见(字数200字以内)"
+        }else
+        {
+            placeholderLabel_.text = "";
+        }
+    }
+    
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool
+    {
+        let newLength = countElements(textView.text);
+        if (newLength>201)
+        {
+            return false;
+        }
+        return true;
+    }
+    
+    //MARK: -UIScrollView Delegate
+    func scrollViewWillBeginDecelerating(scrollView: UIScrollView)
+    {
+        feedbackTextView_.resignFirstResponder();
+    }
+    
+    func mailComposeController(controller: MFMailComposeViewController!, didFinishWithResult result: MFMailComposeResult, error: NSError!)
+    {
+        switch(result.value)
+        {
+        case 0:
+            pointMessageView_.setMessageView(0, withMessage: "邮件发送取消")
+            pointMessageView_.removeMessageView(0);
+            break;
+        case 1:
+            pointMessageView_.setMessageView(0, withMessage: "邮件保存成功")
+            pointMessageView_.removeMessageView(0);
+            break;
+        case 2:
+            pointMessageView_.setMessageView(0, withMessage: "邮件发送成功")
+            pointMessageView_.removeMessageView(0);
+            break;
+        case 3:
+            pointMessageView_.setMessageView(0, withMessage: "邮件发送失败")
+            pointMessageView_.removeMessageView(0);
+            break;
+        default:
+            break;
+        }
+        
+        self.dismissViewControllerAnimated(true, completion: nil);
+    }
 }
