085b87304223904a53987ecfa513cadddfcaa5fd
duan <duan@adminisordemini.home>
2014-12-06 21:54:35 +0800

    修改了关于界面

--- DecorationSteward/AboutMeViewController.swift
+++ DecorationSteward/AboutMeViewController.swift
@@ -8,28 +8,84 @@
 
 import UIKit
 
-class AboutMeViewController: UIViewController {
+let IosAppVersion:NSString =  (NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleShortVersionString") as NSString);//[[[NSBundle mainBundle]
 
-    override func viewDidLoad() {
+class AboutMeViewController: UIViewController
+{
+    var deviceHeight:CGFloat = 0.0;
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        self.addLogoView();
+        self.addCopyrightView();
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func initDeviceHeight()
+    {
+        if (DeviceSystemVersion>7.0)
+        {
+            deviceHeight = 60.0;
+        }else
+        {
+            deviceHeight = 0.0;
+        }
     }
-    */
-
+    
+    func addLogoView()
+    {
+        let viewSize:CGSize = self.view.frame.size;
+        let viewHeight:CGFloat = viewSize.height;
+        let viewWidth:CGFloat = viewSize.width;
+        
+        let logoImageView:UIImageView = UIImageView();
+        let logoImageViewRect:CGRect = CGRectMake((viewWidth-110)/2, 44+deviceHeight+80, 110, 110);
+        logoImageView.frame = logoImageViewRect;
+        logoImageView.layer.cornerRadius = 10.0;
+        logoImageView.layer.masksToBounds = true;
+        logoImageView.image = UIImage(named:"Icon-76@3x");
+        
+        let logoLabel:UILabel = UILabel();
+        let logoLabelRect:CGRect = CGRectMake(0, 44+deviceHeight+80+20+110, viewWidth, 20);
+        logoLabel.frame = logoLabelRect;
+        logoLabel.textAlignment = NSTextAlignment.Center;
+        logoLabel.font = UIFont.systemFontOfSize(14.0);
+        logoLabel.backgroundColor = UIColor.clearColor();
+        logoLabel.textColor = UIColor(red: 51.0/255.0, green: 51.0/255.0, blue: 51.0/255.0, alpha: 1.0);
+        logoLabel.text = "当前版本: "+IosAppVersion;
+        
+        self.view.addSubview(logoImageView);
+        self.view.addSubview(logoLabel);
+    }
+    
+    func addCopyrightView()
+    {
+        let viewSize:CGSize = self.view.frame.size;
+        let viewHeight:CGFloat = viewSize.height;
+        let viewWidth:CGFloat = viewSize.width;
+        
+        let emailLabel:UILabel = UILabel();
+        let emailLabelRect:CGRect = CGRectMake(0, viewHeight - 80 - 30, viewWidth, 20);
+        emailLabel.frame = emailLabelRect;
+        emailLabel.textAlignment = NSTextAlignment.Center;
+        emailLabel.font = UIFont.systemFontOfSize(12.0);
+        emailLabel.backgroundColor = UIColor.clearColor();
+        emailLabel.textColor = UIColor(red: 88.0/255.0, green: 88.0/255.0, blue: 88.0/255.0, alpha: 1.0);
+        emailLabel.text = "联系邮箱: qdurenhongcai@163.com";
+        
+        let copyrightLabel:UILabel = UILabel();
+        let copyrightLabelRect:CGRect = CGRectMake(0, viewHeight - 80, viewWidth, 20);
+        copyrightLabel.frame = copyrightLabelRect;
+        copyrightLabel.textAlignment = NSTextAlignment.Center;
+        copyrightLabel.font = UIFont.systemFontOfSize(12.0);
+        copyrightLabel.backgroundColor = UIColor.clearColor();
+        copyrightLabel.textColor = UIColor(red: 15.0/255.0, green: 15.0/255.0, blue: 15.0/255.0, alpha: 1.0);
+        copyrightLabel.text = "Copyright ©｜装修管家,版权所有";
+        
+        self.view.addSubview(emailLabel);
+        self.view.addSubview(copyrightLabel);
+    }
+    
+    
 }
