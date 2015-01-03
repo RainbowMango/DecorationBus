d174376a0ef75fc35734c6e30a6194918f8fee6e
RainbowMango <qdurenhongcai@163.com>
2015-01-03 23:07:57 +0800

    save order with CoreData

--- DecorationBus/ViewController.swift
+++ DecorationBus/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController, UITabBarControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var totalPaid: UILabel!
     @IBOutlet weak var leftBudget: UILabel!
     @IBOutlet weak var totalBudget: UILabel!
+    
+    var orders_ = [NSManagedObject]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +24,10 @@ class ViewController: UIViewController, UITabBarControllerDelegate, UINavigation
         self.navigationController?.delegate = self
         initCategory()
         setSummaryData()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        orders_ = getOrders()
     }
 
     // view配色方案
@@ -36,6 +43,23 @@ class ViewController: UIViewController, UITabBarControllerDelegate, UINavigation
         totalPaid!.text = "支出总额：\(orderSum)"
         leftBudget!.text = "预算余额：\(budgetSum - orderSum)"
         totalBudget!.text = "预算总额：\(budgetSum)"
+    }
+    
+    // 从CoreData中读取所有订单
+    func getOrders() -> [NSManagedObject] {
+        let appDelegate = UIApplication.sharedApplication().delegate as? AppDelegate
+        let managedObjectContext = appDelegate!.managedObjectContext
+        
+        let fetchRequest = NSFetchRequest(entityName: "Order")
+        
+        var error: NSError?
+        let fetchResult = managedObjectContext!.executeFetchRequest(fetchRequest, error: &error) as [NSManagedObject]?
+        if fetchResult == nil {
+            println("获取数据失败: \(error), \(error!.userInfo)")
+            return [NSManagedObject]()
+        }
+        
+        return fetchResult!
     }
     
     // 初始化类别列表，
