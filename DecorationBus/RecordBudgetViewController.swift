23f83f0d6b0aa7ddcd4490746926f0624d3bae5a
RainbowMango <qdurenhongcai@163.com>
2015-01-20 21:48:19 +0800

    optimize budget record view

--- DecorationBus/RecordBudgetViewController.swift
+++ DecorationBus/RecordBudgetViewController.swift
@@ -297,9 +297,6 @@ class RecordBudgetViewController: FormViewController, FormViewControllerDelegate
     struct Static {
         static let moneyTag = "money"
         static let categories = "categories"
-        static let shopTag = "shop"
-        static let phoneTag = "phone"
-        static let addrTag = "address"
         static let commentsTag = "comments"
         static let button = "button"
     }
@@ -347,21 +344,6 @@ class RecordBudgetViewController: FormViewController, FormViewControllerDelegate
             return
         }
         
-        var shop:       String = ""
-        if let val = self.form.sections[1].rows[0].value {
-            shop = val as String
-        }
-        
-        var phone:      String = ""
-        if let val = self.form.sections[1].rows[1].value {
-            phone = val as String
-        }
-        
-        var address:    String = ""
-        if let val = self.form.sections[1].rows[2].value {
-            address = val as String
-        }
-        
         var comments:   String = ""
         if let val = self.form.sections[1].rows[3].value {
             comments = val as String
@@ -375,9 +357,6 @@ class RecordBudgetViewController: FormViewController, FormViewControllerDelegate
         manageObject.setValue(money, forKey: "money")
         manageObject.setValue(primeCategory, forKey: "primeCategory")
         manageObject.setValue(minorCategory, forKey: "minorCategory")
-        manageObject.setValue(shop, forKey: "shop")
-        manageObject.setValue(phone, forKey: "phone")
-        manageObject.setValue(address, forKey: "address")
         manageObject.setValue(comments, forKey: "comments")
         var error: NSError?
         if false == managedObjectContext!.save(&error) {
@@ -411,18 +390,6 @@ class RecordBudgetViewController: FormViewController, FormViewControllerDelegate
         section1.addRow(row)
         
         let section2 = FormSectionDescriptor()
-        
-        row = FormRowDescriptor(tag: Static.shopTag, rowType: .Name, title: "商家")
-        row.cellConfiguration = ["textField.placeholder" : "商家或品牌", "textField.textAlignment" : NSTextAlignment.Right.rawValue]
-        section2.addRow(row)
-        
-        row = FormRowDescriptor(tag: Static.phoneTag, rowType: .Phone, title: "电话")
-        row.cellConfiguration = ["textField.placeholder" : "商家电话", "textField.textAlignment" : NSTextAlignment.Right.rawValue]
-        section2.addRow(row)
-        
-        row = FormRowDescriptor(tag: Static.addrTag, rowType: .Name, title: "地址")
-        row.cellConfiguration = ["textField.placeholder" : "商家地址", "textField.textAlignment" : NSTextAlignment.Right.rawValue]
-        section2.addRow(row)
         
         row = FormRowDescriptor(tag: Static.commentsTag, rowType: .MultilineText, title: "备注")
         section2.addRow(row)
