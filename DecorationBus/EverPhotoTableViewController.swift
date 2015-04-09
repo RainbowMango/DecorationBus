7f3278b6ee019e233f6bad65654f9efcca0e08bd
RainbowMango <qdurenhongcai@163.com>
2015-04-09 23:05:35 +0800

    override EverPhotoTableViewController with MWPhotoBrowser

--- DecorationBus/EverPhotoTableViewController.swift
+++ DecorationBus/EverPhotoTableViewController.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 
-class EverPhotoTableViewController: UITableViewController {
+class EverPhotoTableViewController: UITableViewController, MWPhotoBrowserDelegate {
 
     var phase_ = ["素颜", "设计", "改造", "水电", "木工", "泥工", "油漆", "墙纸", "软装"]
     var phaseTips_ = ["开始动工前，给您的爱家留个影吧~", "设计图纸存档，方便后期查看哦~", "我的房子我做主，想要几房要房~", "线管走向务必拍照哦，后期打洞洞不小心打到了可不是闹着玩滴~", "除了秀下木工师傅手艺，还能干嘛~", "瓷砖贴上大大提升房主逼格~", "我家的样子开始显现了哦~", "墙纸还是壁画？还是墙纸吧，谁用谁知道~", "哇塞，美得一塌糊涂"]
     
     var selectedAlbum: String = String() // 选中特定相册时设置为相册名字，传递到下一个相册
+
+    var _photos = NSMutableArray()
+    var _thumbs = NSMutableArray()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -72,42 +75,69 @@ class EverPhotoTableViewController: UITableViewController {
 //        var alertView = UIAlertView(title: "敬请期待", message: "你造吗，这是款众多装修业主一起打造的产品，你要不要参与呢~", delegate: self, cancelButtonTitle: "好的")
 //        alertView.show()
         
-        performSegueWithIdentifier("toEverPhotoAlbum", sender: self.view)
+        //performSegueWithIdentifier("toEverPhotoAlbum", sender: self.view)
+        
+        var photo = MWPhoto()
+        var displayActionButton = true
+        var displaySelectionButtons = false
+        var displayNavArrows = false
+        var enableGrid = true
+        var startOnGrid = false
+        
+        var imageURLs = AlbumHandler().getURLList(selectedAlbum)
+        // Photos
+        for imageURL in imageURLs {
+            var myImage = UIImage(contentsOfFile: imageURL)
+            photo = MWPhoto(image: myImage)
+            photo.caption = ""
+            _photos.addObject(photo)
+        }
+        
+        // Thumbs
+        for imageURL in imageURLs {
+            var myImage = UIImage(contentsOfFile: imageURL)
+            photo = MWPhoto(image: myImage)
+            _thumbs.addObject(photo)
+        }
+        
+        // Options
+        startOnGrid = true
+        displayNavArrows = true
+        
+        // Create browser
+        var browser = MWPhotoBrowser(delegate: self)
+        browser.displayActionButton = displayActionButton
+        browser.displayNavArrows = displayNavArrows;
+        browser.displaySelectionButtons = displaySelectionButtons;
+        browser.alwaysShowControls = displaySelectionButtons;
+        browser.zoomPhotosToFill = true;
+//        #if __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_7_0
+//            browser.wantsFullScreenLayout = YES;
+//        #endif
+        browser.enableGrid = enableGrid;
+        browser.startOnGrid = startOnGrid;
+        browser.enableSwipeToDismiss = true;
+        browser.setCurrentPhotoIndex(0)
+        
+        // Reset selections
+        if (displaySelectionButtons) {
+            var _selections = NSMutableArray()
+            for (var i = 0; i < _photos.count; i++) {
+                _selections.addObject(NSNumber(bool: false))
+            }
+        }
+        
+        // Show
+        self.navigationController?.pushViewController(browser, animated: true)
+        
+        // Deselect
+        self.tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        // Test reloading of data after delay
+//        double delayInSeconds = 3;
+//        dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+//        dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
     }
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     // MARK: - Navigation
 
@@ -118,4 +148,33 @@ class EverPhotoTableViewController: UITableViewController {
         }
     }
 
+    func numberOfPhotosInPhotoBrowser(photoBrowser: MWPhotoBrowser!) -> UInt {
+        return UInt(_photos.count)
+    }
+    
+    func photoBrowser(photoBrowser: MWPhotoBrowser!, captionViewForPhotoAtIndex index: UInt) -> MWCaptionView! {
+        if index < UInt(_photos.count) {
+            return _photos.objectAtIndex(Int(index)) as MWCaptionView
+        }
+        return nil
+    }
+    
+    func photoBrowser(photoBrowser: MWPhotoBrowser!, photoAtIndex index: UInt) -> MWPhotoProtocol! {
+        if (index < UInt(_photos.count)) {
+            return _photos.objectAtIndex(Int(index)) as MWPhotoProtocol
+        }
+        return nil;
+    }
+    
+    
+//     override func shouldAutorotateToInterfaceOrientation(toInterfaceOrientation: UIInterfaceOrientation) -> Bool {
+//        return true
+//    }
+    override func prefersStatusBarHidden() -> Bool {
+        return false
+    }
+    
+    override func preferredStatusBarUpdateAnimation() -> UIStatusBarAnimation {
+        return UIStatusBarAnimation.None
+    }
 }
