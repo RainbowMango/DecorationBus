cd7d9eb6dbe1661e02eef8f9540a845c344e28dd
RainbowMango <qdurenhongcai@163.com>
2015-04-18 12:12:10 +0800

    show album info at table view

--- DecorationBus/EverPhotoTableViewController.swift
+++ DecorationBus/EverPhotoTableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class EverPhotoTableViewController: UITableViewController/*, MWPhotoBrowserDelegate*/ {
+class EverPhotoTableViewController: UITableViewController {
 
     var phase_ = ["素颜", "设计", "改造", "水电", "木工", "泥工", "油漆", "墙纸", "软装"]
     var phaseTips_ = ["开始动工前，给您的爱家留个影吧~", "设计图纸存档，方便后期查看哦~", "我的房子我做主，想要几房要房~", "线管走向务必拍照哦，后期打洞洞不小心打到了可不是闹着玩滴~", "除了秀下木工师傅手艺，还能干嘛~", "瓷砖贴上大大提升房主逼格~", "我家的样子开始显现了哦~", "墙纸还是壁画？还是墙纸吧，谁用谁知道~", "哇塞，美得一塌糊涂"]
@@ -19,6 +19,11 @@ class EverPhotoTableViewController: UITableViewController/*, MWPhotoBrowserDeleg
         super.viewDidLoad()
 
         setViewColor()
+    }
+    
+    /*每次展示都刷新tableview，因为相册信息或许有变化*/
+    override func viewWillAppear(animated: Bool) {
+        self.tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -42,12 +47,18 @@ class EverPhotoTableViewController: UITableViewController/*, MWPhotoBrowserDeleg
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
+        let cell = tableView.dequeueReusableCellWithIdentifier("albumTableCell", forIndexPath: indexPath) as! UITableViewCell
 
-        // 配置cell，后期可以自定义cell显示每个相册的图片数量以及添加时间
-        cell.textLabel?.text = phase_[indexPath.section]
-        cell.imageView?.image = UIImage(named: "Album")
-
+        // 配置cell, 显示本相册基本信息并设置图片圆角效果
+        let albumName = phase_[indexPath.section]
+        let imageURLs = AlbumHandler().getURLList(albumName)
+        let imageNumber = imageURLs.count
+        cell.textLabel?.text = albumName
+        cell.detailTextLabel?.text = "\(imageNumber)"
+        cell.imageView?.image = (imageNumber > 0) ? (UIImage(contentsOfFile: imageURLs[0])) : (UIImage(named: "Album"))
+        cell.imageView?.layer.masksToBounds = true
+        cell.imageView?.layer.cornerRadius = 10.0
+        
         return cell
     }
     
