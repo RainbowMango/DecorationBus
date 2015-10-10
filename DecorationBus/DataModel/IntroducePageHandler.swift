7c6cae58fd845e7c435a15b40118fc54c343fb28
RainbowMango <qdurenhongcai@163.com>
2015-10-10 21:58:10 +0800

    sync code from oschina

---
+++ DecorationBus/DataModel/IntroducePageHandler.swift
@@ -0,0 +1,95 @@
+//
+//  IntroducePageHandler.swift
+//  DecorationBus
+//
+//  Created by ruby on 15/6/18.
+//  Copyright (c) 2015年 ruby. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class IntroducePageHandler: NSObject {
+    var resourceFile: String = "IntroducePage"
+    var resourceType: String = "plist"
+    
+    // 设置引导页已展现标记
+    func setIntroShown() -> Void {
+        let curVersion = getCurrentVersion()
+        var plistFile = getSandboxFile()
+        var plistArry = NSMutableArray(contentsOfFile: plistFile)
+        assert(plistArry != nil, "获取类别列表失败")
+        
+        var newItem = NSMutableDictionary()
+        newItem.setObject(curVersion, forKey: "version")
+        newItem.setObject(true, forKey: "shown")
+        
+        plistArry!.addObject(newItem)
+        
+        var rc = plistArry!.writeToFile(plistFile, atomically: true)
+        assert(rc, "写文件失败")
+    }
+    
+    // 检查引导页是否已在当前版本展现
+    func isIntroShown() -> Bool {
+        copyFileToSandbox()
+        
+        let curVersion = getCurrentVersion()
+        
+        var plistArry = NSArray(contentsOfFile: getSandboxFile())
+        assert(plistArry != nil, "获取类别列表失败")
+        
+        for entry in plistArry! {
+            if curVersion == entry["version"] as! String {
+                return entry["shown"] as! Bool
+            }
+        }
+        
+        return false
+    }
+    
+    func getCurrentVersion() -> String {
+        return IosAppVersion as String
+    }
+    
+    // MARK: -- 沙盒文件处理
+    
+    /*拷贝resource文件到沙箱，如沙箱内已存在则忽略*/
+    func copyFileToSandbox() -> Bool {
+        let srcFile = getBundleFile()
+        let dstFile = getSandboxFile()
+        
+        if(NSFileManager().fileExistsAtPath(dstFile)) {
+            return true
+        }
+        
+        var error: NSError?
+        var rc = NSFileManager().copyItemAtPath(srcFile, toPath: dstFile, error: &error)
+        assert(rc, "拷贝列表文件失败")
+        
+        return true
+    }
+    
+    /*获取bundle目录中plist文件*/
+    func getBundleFile() -> String {
+        let file = NSBundle.mainBundle().pathForResource(resourceFile, ofType: resourceType)
+        assert(file != nil, "bundle文件读取失败")
+        
+        return file!
+    }
+    
+    /*获取document目录中plist文件*/
+    func getSandboxFile() -> String {
+        var docPath = getDocumentDirectory()
+        var file = docPath.stringByAppendingPathComponent("\(resourceFile).plist")
+        
+        return file
+    }
+    
+    /*获取document目录*/
+    func getDocumentDirectory() -> String {
+        let directories = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true) as! Array<String>
+        assert(directories.count > 0, "获取document目录失败")
+        return directories[0]
+    }
+}
