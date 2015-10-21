e3af4d36207b557c240f3fd66dcd66a6fd991ef3
RainbowMango <qdurenhongcai@163.com>
2015-10-21 23:22:41 +0800

    clear index page

--- DecorationBus/ViewController.swift
+++ DecorationBus/ViewController.swift
@@ -10,13 +10,6 @@ import UIKit
 import CoreData
 
 class ViewController: UIViewController, UITabBarControllerDelegate, UINavigationControllerDelegate {
-
-    @IBOutlet weak var totalPaid: UILabel!
-    @IBOutlet weak var leftBudget: UILabel!
-    @IBOutlet weak var totalBudget: UILabel!
-    
-    var orders_ = [NSManagedObject]()
-    var budgets_ = [NSManagedObject]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,7 +22,7 @@ class ViewController: UIViewController, UITabBarControllerDelegate, UINavigation
     }
     
     override func viewDidAppear(animated: Bool) {
-        setSummaryData()
+        
     }
     
     // 启动系统引导页
@@ -74,74 +67,6 @@ class ViewController: UIViewController, UITabBarControllerDelegate, UINavigation
         self.view.backgroundColor = ColorScheme().viewBackgroundColor
     }
     
-    // 设置汇总数据
-    func setSummaryData() {
-        orders_ = getOrders()
-        budgets_ = getBudgets()
-        
-        let orderSum = getOrderSum()
-        let budgetSum = getBudgetSum()
-        
-        totalPaid!.text = "支出总额：\(orderSum)"
-        leftBudget!.text = "预算余额：\(budgetSum - orderSum)"
-        totalBudget!.text = "预算总额：\(budgetSum)"
-    }
-    
-    // 获取订单金额汇总
-    func getOrderSum() -> Float {
-        var sum: Float = 0.00
-        for item in orders_ {
-            sum += item.valueForKey("money") as! Float
-        }
-        
-        return sum
-    }
-    
-    // 获取预算金额汇总
-    func getBudgetSum() -> Float {
-        var sum: Float = 0.00
-        for item in budgets_ {
-            sum += item.valueForKey("money") as! Float
-        }
-        
-        return sum
-    }
-    
-    // 从CoreData中读取所有订单
-    func getOrders() -> [NSManagedObject] {
-        let appDelegate = UIApplication.sharedApplication().delegate as? AppDelegate
-        let managedObjectContext = appDelegate!.managedObjectContext
-        
-        let fetchRequest = NSFetchRequest(entityName: "Order")
-        
-        var fetchResult = [NSManagedObject]()
-        do {
-            fetchResult = try managedObjectContext!.executeFetchRequest(fetchRequest) as! [NSManagedObject]
-        }catch {
-            print("获取数据失败:")
-            return [NSManagedObject]()
-        }
-        
-        return fetchResult
-    }
-    
-    // 从CoreData中读取所有预算
-    func getBudgets() -> [NSManagedObject] {
-        let appDelegate = UIApplication.sharedApplication().delegate as? AppDelegate
-        let managedObjectContext = appDelegate!.managedObjectContext
-        
-        let fetchRequest = NSFetchRequest(entityName: "Budget")
-        var fetchResult = [NSManagedObject]()
-        do {
-            fetchResult = try managedObjectContext!.executeFetchRequest(fetchRequest) as! [NSManagedObject]
-        }catch {
-            print("获取数据失败:")
-            return [NSManagedObject]()
-        }
-        
-        return fetchResult
-    }
-    
     // 初始化类别列表，程序启动时拷贝资源文件到沙盒
     func initCategory() {
         CategoryHandler().copyFileToSandbox()
@@ -159,12 +84,12 @@ class ViewController: UIViewController, UITabBarControllerDelegate, UINavigation
     
     // tab bar 切换时重新加载数据
     func tabBarController(tabBarController: UITabBarController, didSelectViewController viewController: UIViewController) {
-        setSummaryData()
+        
     }
     
     // 导航回来时刷新数据
     func navigationController(navigationController: UINavigationController, didShowViewController viewController: UIViewController, animated: Bool) {
-        setSummaryData()
+        
     }
 }
 
