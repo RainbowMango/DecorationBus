ce2a10a8f9b61c30bc37f3bd66fe9f9430edff08
RainbowMango <qdurenhongcai@163.com>
2015-11-07 23:25:56 +0800

    Remove IntroducePageHandler class as it has been replaced by user default solution. IntroducePage.plist has been removed as well. Remove corresponding modules in ViewController.

--- DecorationBus/ViewController.swift
+++ DecorationBus/ViewController.swift
@@ -48,46 +48,6 @@ class ViewController: UIViewController, UITabBarControllerDelegate, UINavigation
         
         // 启动友盟统计
         MobClick.endLogPageView("首页")
-        
-        //startIntro() // 最后显示引导页，防止其他view遮挡
-    }
-    
-    // 启动系统引导页
-    func startIntro() -> Void {
-        //判断是否第一次启动，如是则跳过引导
-        if IntroducePageHandler().isIntroShown() {
-            return
-        }
-        
-        UIApplication.sharedApplication().statusBarHidden = true
-        self.navigationController?.navigationBarHidden = true
-        self.tabBarController?.tabBar.hidden = true
-
-        let item1 = RMParallaxItem(image: UIImage(named: "intr001")!, text: "装修路上处处陷阱...")
-        let item2 = RMParallaxItem(image: UIImage(named: "intr002")!, text: "低价往往不是让利促销...")
-        let item3 = RMParallaxItem(image: UIImage(named: "intr003")!, text: "设计师的时间总是伴随着利益...")
-        let item4 = RMParallaxItem(image: UIImage(named: "intr004")!, text: "装修增项让人防不胜防...")
-        let item5 = RMParallaxItem(image: UIImage(named: "intr005")!, text: "我们, 只是想让装修更简单一点点...")
-        let rmParallaxViewController = RMParallax(items: [item1, item2, item3, item4, item5], motion: false)
-        
-        //定义结束引导页行为，显示导航栏，TAB栏
-        rmParallaxViewController.completionHandler = {
-            UIApplication.sharedApplication().statusBarHidden = false
-            self.navigationController?.navigationBarHidden = false
-            self.tabBarController?.tabBar.hidden = false
-            UIView.animateWithDuration(0.4, animations: { () -> Void in
-
-                rmParallaxViewController.view.alpha = 0.0
-            })
-        }
-        
-        // Adding parallax view controller.
-        self.addChildViewController(rmParallaxViewController)
-        self.view.addSubview(rmParallaxViewController.view)
-        rmParallaxViewController.didMoveToParentViewController(self)
-        
-        //增加标志以让下次启动不再显示引导页
-        IntroducePageHandler().setIntroShown()
     }
 
     // view配色方案
