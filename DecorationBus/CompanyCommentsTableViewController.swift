557b81c52176eb0c00bb323d02fcf84dacfadd7c
RainbowMango <qdurenhongcai@163.com>
2015-12-13 22:35:59 +0800

    Request company comments when view load.

--- DecorationBus/CompanyCommentsTableViewController.swift
+++ DecorationBus/CompanyCommentsTableViewController.swift
@@ -18,14 +18,16 @@ class CompanyCommentsTableViewController: UITableViewController {
         
         self.tableView.tableFooterView = UIView() // 清除tableView中空白行
         
-        //添加测试数据
-        let comment = CompanyComment()
-        comment.avatar   = "http://decorationbus.sinaapp.com/server/images/banner/ser01.jpg"
-        comment.nickname = "用户昵称"
-        comment.date     = "2015-12-11"
-        comment.score    = 87
-        comment.comment  = "还不错哦还不错哦还不错哦还不错哦还不错哦还不错哦还不错哦还不错哦还不错哦"
-        self._comments.append(comment)
+        self._comments = requestCompanyComments(0, companyId: self._company.id)
+        
+//        //添加测试数据
+//        let comment = CompanyComment()
+//        comment.avatar   = "http://decorationbus.sinaapp.com/server/images/banner/ser01.jpg"
+//        comment.nickname = "用户昵称"
+//        comment.date     = "2015-12-11"
+//        comment.score    = 87
+//        comment.comment  = "还不错哦还不错哦还不错哦还不错哦还不错哦还不错哦还不错哦还不错哦还不错哦"
+//        self._comments.append(comment)
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,7 +42,7 @@ class CompanyCommentsTableViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return self._comments.count
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -96,4 +98,51 @@ class CompanyCommentsTableViewController: UITableViewController {
     }
     */
 
+    // MARK: - Request And Refresh
+    func requestCompanyComments(counter: Int, companyId: UInt) -> Array<CompanyComment> {
+        let urlStr = REQUEST_COMPANY_COMMENTS_URL_STR + "?counter=\(counter)" + "&company=\(companyId)"
+        let url = NSURL(string: urlStr)
+        let request = NSURLRequest(URL: url!)
+        do {
+            let data = try NSURLConnection.sendSynchronousRequest(request, returningResponse: nil)
+            return parseComanyComments(data)
+        }catch let error as NSError{
+            print("网络异常--请求公司评论信息失败：" + error.localizedDescription)
+        }
+        
+        return Array<CompanyComment>()
+    }
+    
+    // 解析请求到的JSON数据
+    func parseComanyComments(jsonData: NSData) -> Array<CompanyComment> {
+        do {
+            let jsonStr = try NSJSONSerialization.JSONObjectWithData(jsonData, options: NSJSONReadingOptions.AllowFragments)
+            let itemNum = jsonStr.objectForKey("total") as! Int
+            let items = jsonStr.objectForKey("row") as! NSArray
+            
+            var requestedCompanyComments = Array<CompanyComment>()
+            for item in items {
+                let comment = CompanyComment()
+                comment.avatar   = item.objectForKey("avatar") as! String
+                comment.nickname = item.objectForKey("nickname") as! String
+                comment.date     = item.objectForKey("date") as! String
+                comment.comment  = item.objectForKey("comment") as! String
+                comment.score    = item.objectForKey("score") as! UInt
+                requestedCompanyComments.append(comment)
+            }
+            
+            //服务端返回的JSON有误时仅打印一条信息
+            if(itemNum != requestedCompanyComments.count) {
+                print("Warning: items number mismatch in json!")
+            }
+            
+            return requestedCompanyComments
+        }catch let error as NSError {
+            print("解析JSON数据失败: " + error.localizedDescription)
+        }
+        
+        return Array<CompanyComment>()
+    }
+
+    
 }
