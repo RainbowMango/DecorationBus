dbb6ecd9dded109bc8b5cff4d162c9199c316b93
RainbowMango <qdurenhongcai@163.com>
2015-12-19 12:40:01 +0800

    Add images in company comments table view cell

--- DecorationBus/CustomCell/CompanyCommentTableViewCell.swift
+++ DecorationBus/CustomCell/CompanyCommentTableViewCell.swift
@@ -14,6 +14,7 @@ class CompanyComment {
     var date     : String
     var comment  : String
     var score    : UInt
+    var images   : Array<String>
     
     init() {
         avatar   = String()
@@ -21,9 +22,11 @@ class CompanyComment {
         date     = String()
         comment  = String()
         score    = 0
+        images   = Array<String>()
     }
 }
 
+var j = 0
 class CompanyCommentTableViewCell: UITableViewCell {
     @IBOutlet weak var avatar: UIImageView!
     @IBOutlet weak var nickname: UILabel!
@@ -31,6 +34,10 @@ class CompanyCommentTableViewCell: UITableViewCell {
     @IBOutlet weak var scoreChart: MDRadialProgressView!
     @IBOutlet weak var comment: UILabel!
     @IBOutlet weak var imageSection: UIView!
+    @IBOutlet weak var image1: UIImageView!
+    @IBOutlet weak var image2: UIImageView!
+    @IBOutlet weak var image3: UIImageView!
+    @IBOutlet weak var image4: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -57,6 +64,9 @@ class CompanyCommentTableViewCell: UITableViewCell {
         
         //设置分数
         configureScore(data.score)
+        
+        //设置图片
+        configureImages(data.images)
     }
     
     func configureAvatar(imagePath: String) -> Void {
@@ -89,5 +99,52 @@ class CompanyCommentTableViewCell: UITableViewCell {
         self.scoreChart.theme = newTheme
         self.scoreChart.progressTotal = 100
         self.scoreChart.progressCounter = score
+    }
+    
+    func configureImages(var images: Array<String>) -> Void {
+//        if(i % 2 == 0) {
+//            let imageURL = NSURL(string: "http://decorationbus.sinaapp.com/server/images/banner/ser01.jpg")
+//            self.image1.sd_setImageWithURL(imageURL, placeholderImage: UIImage(named: "companyDefaultLogo.png"))
+//        }
+        j++
+        images.append("http://decorationbus.sinaapp.com/server/images/banner/ser01.jpg")
+        images.append("http://decorationbus.sinaapp.com/server/images/banner/ser02.jpg")
+        images.append("http://decorationbus.sinaapp.com/server/images/banner/ser03.jpg")
+        images.append("http://decorationbus.sinaapp.com/server/images/banner/ser04.jpg")
+        if(j % 2 == 0) {
+            return
+        }
+        for(var i = 0; i < images.count; i++) {
+            let imageURL = NSURL(string: images[i])
+            let imageView = self.imageSection.subviews[i].subviews[0] as! UIImageView
+            imageView.sd_setImageWithURL(imageURL, placeholderImage: UIImage(named: "companyDefaultLogo.png"))
+        }
+        return
+        //如果没有图片则隐藏图片区
+        if(images.isEmpty) {
+            //self.imageSection.hidden = true
+            //self.imageSection.frame = CGRect.zero
+        }
+        
+        if(images.count > 0) {
+            let imageURL = NSURL(string: images[0])
+            self.image1.sd_setImageWithURL(imageURL, placeholderImage: UIImage(named: "companyDefaultLogo.png"))
+        }
+        
+        if(images.count > 1) {
+            let imageURL = NSURL(string: images[1])
+            self.image2.sd_setImageWithURL(imageURL, placeholderImage: UIImage(named: "companyDefaultLogo.png"))
+        }
+        
+        if(images.count > 2) {
+            let imageURL = NSURL(string: images[2])
+            self.image2.sd_setImageWithURL(imageURL, placeholderImage: UIImage(named: "companyDefaultLogo.png"))
+        }
+        
+        
+        if(images.count > 3) {
+            let imageURL = NSURL(string: images[3])
+            self.image2.sd_setImageWithURL(imageURL, placeholderImage: UIImage(named: "companyDefaultLogo.png"))
+        }
     }
 }
