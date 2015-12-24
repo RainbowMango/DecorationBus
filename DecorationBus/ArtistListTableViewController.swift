61df2abfa6b350a74e997b8b682e3e4e38d67180
RainbowMango <qdurenhongcai@163.com>
2015-12-25 00:24:07 +0800

    Basic function done: show artist list.

--- DecorationBus/ArtistListTableViewController.swift
+++ DecorationBus/ArtistListTableViewController.swift
@@ -9,15 +9,29 @@
 import UIKit
 
 class ArtistListTableViewController: UITableViewController {
-
+    
+    var tableHeader = MJRefreshNormalHeader()
+    var tableFooter = MJRefreshAutoNormalFooter()
+    var artists     = Array<ArtistCellData>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        //解决tableview和导航之间留白，留白尺寸=状态栏高度+导航栏的高度+10
+        let navbarHeight = self.navigationController?.navigationBar.frame.height
+        let statusBarHeight = UIApplication.sharedApplication().statusBarFrame.height
+        self.tableView.contentInset = UIEdgeInsetsMake(0 + 10 - statusBarHeight - navbarHeight!, 0, 0, 0)
+        
+        //添加下拉刷新控件
+        tableHeader = MJRefreshNormalHeader(refreshingTarget: self, refreshingAction: "tableHeaderRefresh")
+        tableHeader.automaticallyChangeAlpha = true
+        self.tableView.tableHeaderView = tableHeader
+        
+        //添加上拉刷新控件
+        tableFooter = MJRefreshAutoNormalFooter(refreshingTarget: self, refreshingAction: "tableFooterRefresh")
+        self.tableView.tableFooterView = tableFooter
+        
+        self.tableHeader.beginRefreshing()
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,24 +42,27 @@ class ArtistListTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.artists.count
     }
 
-    /*
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
+        //从队列中获取cell
+        let cell = tableView.dequeueReusableCellWithIdentifier("artistItem", forIndexPath: indexPath) as! ArtistTableViewCell
+        
+        // 更新cell数据
+        if(indexPath.row > self.artists.count) {
+            print("警告：当前source数量\(self.artists.count), 当前cell行数\(indexPath.row)")
+            return cell
+        }
+        
+        cell.configureViews(self.artists[indexPath.row])
+        
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
@@ -91,5 +108,87 @@ class ArtistListTableViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    // MARK: - Refresh
+    
+    func requestArtists(counter: Int) -> Array<ArtistCellData> {
+        let urlStr = REQUEST_ARTISTS_URL_STR + "?counter=\(counter)"
+        let url = NSURL(string: urlStr)
+        let request = NSURLRequest(URL: url!)
+        do {
+            let data = try NSURLConnection.sendSynchronousRequest(request, returningResponse: nil)
+            return parseArtists(data)
+        }catch let error as NSError{
+            print("网络异常--请求公司信息失败：" + error.localizedDescription)
+        }
+        
+        return Array<ArtistCellData>()
+    }
+    
+    // 解析请求到的JSON数据
+    func parseArtists(jsonData: NSData) -> Array<ArtistCellData> {
+        do {
+            let jsonStr = try NSJSONSerialization.JSONObjectWithData(jsonData, options: NSJSONReadingOptions.AllowFragments)
+            let itemNum = jsonStr.objectForKey("total") as! Int
+            let items = jsonStr.objectForKey("row") as! NSArray
+            
+            var requestedArtists = Array<ArtistCellData>()
+            for item in items {
+                let artist = ArtistCellData()
+                artist.id = item.objectForKey("id") as! UInt
+                artist.name = item.objectForKey("name") as! String
+                artist.avatar = item.objectForKey("avatar") as! String
+                artist.companyName = item.objectForKey("company") as! String
+                artist.commentsNum = item.objectForKey("comments") as! UInt
+                artist.score = item.objectForKey("score") as! UInt
+                requestedArtists.append(artist)
+            }
+            
+            //服务端返回的JSON有误时仅打印一条信息
+            if(itemNum != requestedArtists.count) {
+                print("Warning: items number mismatch in json!")
+            }
+            
+            return requestedArtists
+        }catch let error as NSError {
+            print("解析JSON数据失败: " + error.localizedDescription)
+        }
+        
+        return Array<ArtistCellData>()
+    }
+    
+    //下拉刷新
+    func tableHeaderRefresh() {
+        //注意：改变data source和reload务必放到一个queue里执行，否则reload时有一定几率crash
+        dispatch_async(dispatch_get_main_queue()) { () -> Void in
+            let requestedArtists = self.requestArtists(0)
+            
+            //只有在成功请求到数据时才改变数据源
+            if(!requestedArtists.isEmpty) {
+                self.artists = requestedArtists
+            }
+            
+            self.tableHeader.endRefreshing()
+            self.tableView.reloadData()//重新请求数据
+        }
+        
+        //重置没有更多的数据（消除没有更多数据的状态）
+        self.tableFooter.resetNoMoreData()
+    }
+    
+    //上拉刷新
+    func tableFooterRefresh() {
+        // 追加每次请求到的数据
+        let requestedArtists = requestArtists(self.artists.count)
+        if(requestedArtists.isEmpty) {
+            self.tableFooter.endRefreshingWithNoMoreData()
+            return
+        }
+        for artist in requestedArtists {
+            self.artists.append(artist)
+        }
+        
+        self.tableFooter.endRefreshing()
+        self.tableView.reloadData()
+    }
 }
--- DecorationBus/CustomCell/ArtistTableViewCell.swift
+++ DecorationBus/CustomCell/ArtistTableViewCell.swift
@@ -12,12 +12,14 @@ class ArtistCellData {
     var id: UInt
     var name: String
     var avatar: String
+    var companyName: String
     var commentsNum: UInt
     var score: UInt
     
     init() {
         avatar = String()
         name = String()
+        companyName = String()
         commentsNum = 0
         score = 0
         id = 0
@@ -25,6 +27,11 @@ class ArtistCellData {
 }
 
 class ArtistTableViewCell: UITableViewCell {
+    @IBOutlet weak var avatar: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var company: UILabel!
+    @IBOutlet weak var commentsNum: UILabel!
+    @IBOutlet weak var scoreChart: MDRadialProgressView!
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -36,5 +43,54 @@ class ArtistTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-
+    
+    func configureViews(data: ArtistCellData) -> Void {
+        //设置头像
+        configureLogo(data.avatar)
+        
+        //设置名字
+        configureName(data.name)
+        
+        //设置公司
+        configureCompany(data.companyName)
+        
+        //设置评价数
+        configureCommentsNum(data.commentsNum)
+        
+        //设置评价图表
+        configureScoreChart(data.score)
+    }
+    
+    func configureLogo(imagePath: String) -> Void {
+        //使用SDWebImage可以自动缓存图片，浏览更流畅
+        let url = NSURL(string: imagePath)
+        self.avatar.sd_setImageWithURL(url, placeholderImage: UIImage(named: "companyDefaultLogo.png"))
+    }
+    
+    func configureName(name: String) -> Void {
+        self.name.text = name
+    }
+    
+    func configureCompany(company: String) -> Void {
+        self.company.text = "所属公司: \(company)"
+    }
+    
+    func configureCommentsNum(num: UInt) -> Void {
+        self.commentsNum.text = "评价数: \(num)"
+    }
+    
+    func configureScoreChart(score: UInt) -> Void {
+        //自定义主题
+        let newTheme = MDRadialProgressTheme()
+        newTheme.completedColor = UIColor.greenColor()
+        newTheme.incompletedColor = UIColor.grayColor()
+        newTheme.centerColor = UIColor.clearColor()
+        newTheme.sliceDividerHidden = true
+        newTheme.labelColor = UIColor.blackColor()
+        newTheme.labelShadowColor = UIColor.whiteColor()
+        
+        self.scoreChart.theme = newTheme
+        self.scoreChart.progressTotal = 100
+        self.scoreChart.progressCounter = score
+    }
 }
