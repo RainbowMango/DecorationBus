943cc14844954573562fdb5d593205e56310c780
RainbowMango <qdurenhongcai@163.com>
2015-12-31 00:13:47 +0800

    add manager comments controller

--- DecorationBus/ManagerTableViewController.swift
+++ DecorationBus/ManagerTableViewController.swift
@@ -72,13 +72,13 @@ class ManagerTableViewController: UITableViewController {
     // MARK: - Navigation
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-//        let dstVC       = segue.destinationViewController as! ArtistCommentsTableViewController
-//        let indexPath   = self.tableView.indexPathForSelectedRow
-//        self.tableView.deselectRowAtIndexPath(indexPath!, animated: true)
-//        dstVC.hidesBottomBarWhenPushed = true
-//        
-//        // 传递选中的cell信息到下一个view
-//        dstVC._artist = artists[indexPath!.row]
+        let dstVC       = segue.destinationViewController as! ManagerCommentsTableViewController
+        let indexPath   = self.tableView.indexPathForSelectedRow
+        self.tableView.deselectRowAtIndexPath(indexPath!, animated: true)
+        dstVC.hidesBottomBarWhenPushed = true
+        
+        // 传递选中的cell信息到下一个view
+        dstVC._manager = managers[indexPath!.row]
     }
     
     // MARK: - Refresh
