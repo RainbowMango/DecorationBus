4a319748d2e2a7f2130d5def355a89252d98cbbb
RainbowMango <qdurenhongcai@163.com>
2016-02-01 15:27:13 +0800

    Add hint when user waiting sms verification code

--- DecorationBus/VerifyViewController.swift
+++ DecorationBus/VerifyViewController.swift
@@ -15,24 +15,35 @@ class VerifyViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var verificationCodeField: UITextField!
     @IBOutlet weak var hintLabel: UILabel!
     @IBOutlet weak var sendVerButton: UIButton!
+    @IBOutlet weak var verifyButton: UIButton!
 
     var phoneNumber = String() //存储用户手机号码，防止用户输入验证码时误修改手机号，导致用户体验下降
     
     let phoneNumberFieldTag = 110
     let verificationCodeFieldTag = 120
     
+    var hintTimer = NSTimer()
+    var timerCount = 0
+    
     override func viewDidLoad() {
+        super.viewDidLoad()
+        
         //设置发送验证码button的初始边框
         self.sendVerButton.layer.borderWidth = 0.5
         self.disableSendButton()
         
-        super.viewDidLoad()
+        self.hintLabel.hidden     = true
+        self.verifyButton.enabled = false
 
         //设置输入框
         self.phoneNumberField.tag           = self.phoneNumberFieldTag
         self.phoneNumberField.delegate      = self
         self.verificationCodeField.tag      = self.verificationCodeFieldTag
         self.verificationCodeField.delegate = self
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        self.hintTimer.invalidate()
     }
 
     override func didReceiveMemoryWarning() {
@@ -44,6 +55,7 @@ class VerifyViewController: UIViewController, UITextFieldDelegate {
         self.phoneNumber = self.phoneNumberField.text!
         print("开始向\(phoneNumber)发送验证码！")
         disableSendButton()
+        startHint()
         
         if(11 == self.phoneNumber.lengthOfBytesUsingEncoding(NSASCIIStringEncoding)) {
             SMSSDK.getVerificationCodeByMethod(SMSGetCodeMethodSMS, phoneNumber: phoneNumber, zone: "86", customIdentifier: nil, result: { (error) -> Void in
@@ -61,6 +73,7 @@ class VerifyViewController: UIViewController, UITextFieldDelegate {
 
     @IBAction func verifyPhoneNumber(sender: AnyObject) {
         let verCode = self.verificationCodeField.text
+        stopHint()
         if(verCode?.lengthOfBytesUsingEncoding(NSASCIIStringEncoding) > 0) {
             SMSSDK.commitVerificationCode(verCode, phoneNumber: self.phoneNumberField.text, zone: "86", result: { (error) -> Void in
                 if(error != nil) {
@@ -113,6 +126,37 @@ class VerifyViewController: UIViewController, UITextFieldDelegate {
         return UserInfo()
     }
     
+    /*
+    * 启动计时器并显示等待秒数
+    */
+    func startHint() {
+        self.hintTimer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "updateHintTime", userInfo: nil, repeats: true)
+        self.hintLabel.hidden = false
+        self.timerCount = 0
+    }
+    
+    /*
+    * 停止计时器
+    */
+    func stopHint() {
+        self.hintTimer.invalidate()
+        self.hintLabel.hidden = true
+        self.timerCount = 0
+    }
+    
+    func updateHintTime() {
+        //60秒还未收到短信验证码，提示用户重试
+        if(timerCount >= 60) {
+            hintTimer.invalidate()
+            print("计时器已经超时")
+            return
+        }
+        
+        print("计时时间已到\(self.timerCount)")
+        self.timerCount++
+        self.hintLabel.text = "还需要等待\(60 - timerCount)秒"
+    }
+    
     // MARK: - UITextFieldDelegate
     
     /*
@@ -125,17 +169,24 @@ class VerifyViewController: UIViewController, UITextFieldDelegate {
         
         switch textField.tag {
         case self.phoneNumberFieldTag:
-            guard fieldLength <= 11 else {
+            guard fieldLength <= 11 else { //不允许输入超过11位
                 return false
             }
             
             if(fieldLength == 11) {
                 enableSendButton()
             }
+            else {
+                disableSendButton()
+            }
+            
         case self.verificationCodeFieldTag:
             if(fieldLength >= 4) {
-                //TODO 激活验证按钮
+                self.verifyButton.enabled = true
             }
+            
+            guard fieldLength < 10 else {return false}
+            
         default:
             print("未处理的UITextFiled, tag= \(textField.tag)")
             
