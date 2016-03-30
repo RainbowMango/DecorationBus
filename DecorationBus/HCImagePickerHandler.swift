c3584b825c4ad47191758a753ea50d5cded6ea9f
RainbowMango <qdurenhongcai@163.com>
2016-03-30 22:59:21 +0800

    add HCImagePickerHandler intend to handle all image pick tasks.

---
+++ DecorationBus/HCImagePickerHandler.swift
@@ -0,0 +1,17 @@
+//
+//  HCImagePickerHandler.swift
+//  DecorationBus
+//
+//  Created by ruby on 16/3/30.
+//  Copyright © 2016年 ruby. All rights reserved.
+//
+
+import Foundation
+
+class HCImagePickerHandler {
+    
+    // 定义照片源字符串，方便创建actionSheet和处理代理
+    let actionSheetTitleCancel = "取消"
+    let actionSheetTitleCamera = "拍照"
+    let actionSheetTitlePhotoLibrary = "照片库"
+}
