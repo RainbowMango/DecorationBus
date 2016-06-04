8ceadf937aaa50e7a5eaaf375c1de1a73c8fc379
RainbowMango <qdurenhongcai@163.com>
2016-06-04 23:31:24 +0800

    Add hint when user trying to modify account.

--- DecorationBus/AccountSettingTableViewController.swift
+++ DecorationBus/AccountSettingTableViewController.swift
@@ -19,12 +19,6 @@ class AccountSettingTableViewController: UITableViewController {
         userInfo = UserDataHandler().getUserInfoFromConf()
         
         tableView.tableFooterView = UIView() // 清除tableView中空白行
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
@@ -77,49 +71,25 @@ class AccountSettingTableViewController: UITableViewController {
         }
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    // MARK: - Table view delegate
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        switch indexPath.row {
+        case 0:
+            showSimpleAlert(self, title: "客官别急", message: "个人信息修改功能还在开发中...")
+            break
+        case 1:
+            showSimpleAlert(self, title: "客官别急", message: "个人信息修改功能还在开发中...")
+            break
+        case 2:
+            showSimpleAlert(self, title: "客官别急", message: "个人信息修改功能还在开发中...")
+            break
+        case 3:
+            showSimpleAlert(self, title: "客官别急", message: "个人信息修改功能还在开发中...")
+            break
+        default:
+            return
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
